1847e30ccf0e72fa41fc873131f916322797f9cb
priyanka <priyanka.chavan1408@gmail.com>
2022-02-12 23:47:28 +0530

    ADDED:Welcome message on master branch.

---
+++ LineComparisonProblem.java
@@ -0,0 +1,13 @@
+package com.line.comparison;
+
+public class LineComparisonProblem {
+
+    public void welcomeOnMasterBranch() {
+        System.out.println("Welcome to the employee wage computation program.");
+    }
+
+    public static void main(String[] args) {
+        LineComparisonProblem lineComparisonProblem =new LineComparisonProblem();
+        lineComparisonProblem.welcomeOnMasterBranch();
+    }
+}
